66fa5fd3ce5a3fb3485532556e9c96090cb78eea
Matheus Silva Melo <matheus-melo21@hotmail.com>
2025-02-20 16:58:09 -0300

    remoção do campo 'editora' na classe Livros

--- src/utils/db/database.py
+++ src/utils/db/database.py
@@ -14,10 +14,9 @@ class Autores(SQLModel, table=True):
 
 class Editoras(SQLModel, table=True):
     idEditora: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    nome: Optional[str] | None = None 
-
+    nome: Optional[str] | None = None
+    
     livros: List["Livros"] = Relationship(back_populates="editora")
-
 
 
 class Livros(SQLModel, table=True):
@@ -25,7 +24,6 @@ class Livros(SQLModel, table=True):
     isbn: str = Field(index=True)
     titulo: str = Field(index=True)
     sinopse: Optional[str] | None = None
-    editora: Optional[str] | None = None
     formato: Optional[str] | None = None
     ano: Optional[int] | None = None
     paginas: Optional[int] | None = None
@@ -34,7 +32,6 @@ class Livros(SQLModel, table=True):
     autores: List["Autores"] = Relationship(back_populates="livros", link_model=LivrosAutor)
     idEditora: Optional[uuid.UUID] = Field(default=None, foreign_key="editoras.idEditora")
     editora: Optional["Editoras"] = Relationship(back_populates="livros")
-
 
 
 sqlite_file_name = "database.db"
